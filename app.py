b9efee41ad7588e590d6ca9cd1dd51b98e42e89f
Yuganksharma26 <yuganksharma26@gmail.com>
2025-10-29 23:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "✅ Backend is Live on Render!"
+    return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+if __name__ == '__main__':
+    app.run()
