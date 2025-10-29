4b1dc61e55d9bdb64abd67c4fa8b523051a9121b
Yuganksharma26 <yuganksharma26@gmail.com>
2025-10-29 23:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
